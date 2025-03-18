396b30f8e0de563c204d0c1e7f2984e09cdc4be9
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2025-03-18 23:32:49 +0700

    menambahkan class Course

--- tpmodul7_kelompok_6/Program.cs
+++ tpmodul7_kelompok_6/Program.cs
@@ -24,34 +24,75 @@ namespace tpmodul7_kelompok_6
             public string Fakultas { get; set; }
         }
 
-        public static void ReadJSON()
+        public class Course
         {
-            string filePath = "tp7_1_2211104027.json";
+            public string Code { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class CourseData
+        {
+            public List<Course> Courses { get; set; }
+        }
+
+        public static void ReadJSONMahasiswa()
+        {
+            string filePath = "tp7_1_2211104027.json"; 
 
             if (File.Exists(filePath))
             {
                 try
                 {
                     string jsonData = File.ReadAllText(filePath);
-
                     DataMahasiswa mahasiswa = JsonConvert.DeserializeObject<DataMahasiswa>(jsonData);
 
                     Console.WriteLine($"Nama {mahasiswa.Nama.Depan} {mahasiswa.Nama.Belakang} dengan nim {mahasiswa.NIM} dari fakultas {mahasiswa.Fakultas}");
                 }
                 catch (JsonReaderException ex)
                 {
-                    Console.WriteLine("Terjadi kesalahan saat membaca JSON: " + ex.Message);
+                    Console.WriteLine("Terjadi kesalahan saat membaca JSON Mahasiswa: " + ex.Message);
                 }
             }
             else
             {
-                Console.WriteLine("File JSON tidak ditemukan!");
+                Console.WriteLine("File JSON Mahasiswa tidak ditemukan!");
+            }
+        }
+
+        public static void ReadJSONCourses()
+        {
+            string filePath = "tp7_2_2211104027.json"; 
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    string jsonData = File.ReadAllText(filePath);
+                    CourseData data = JsonConvert.DeserializeObject<CourseData>(jsonData);
+
+                    Console.WriteLine("\nDaftar mata kuliah yang diambil:");
+                    int index = 1;
+                    foreach (var course in data.Courses)
+                    {
+                        Console.WriteLine($"MK {index} {course.Code} - {course.Name}");
+                        index++;
+                    }
+                }
+                catch (JsonReaderException ex)
+                {
+                    Console.WriteLine("Terjadi kesalahan saat membaca JSON Mata Kuliah: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File JSON Mata Kuliah tidak ditemukan!");
             }
         }
 
         static void Main()
         {
-            ReadJSON();
+            ReadJSONMahasiswa();
+            ReadJSONCourses();
         }
     }
 }
