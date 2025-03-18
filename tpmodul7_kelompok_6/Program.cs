d98114fdf3a30b3fe48a7e6637e3f76f6ee4b269
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2025-03-18 23:18:39 +0700

    menambahkan class DataMahasiswa2211104027

--- tpmodul7_kelompok_6/Program.cs
+++ tpmodul7_kelompok_6/Program.cs
@@ -3,13 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
 
 namespace tpmodul7_kelompok_6
 {
     class Program
     {
-        static void Main(string[] args)
+        public class Nama
         {
+            public string Depan { get; set; }
+            public string Belakang { get; set; }
+        }
+
+        public class DataMahasiswa
+        {
+            public Nama Nama { get; set; }
+            public long NIM { get; set; }
+            public string Fakultas { get; set; }
+        }
+
+        public static void ReadJSON()
+        {
+            string filePath = "tp7_1_2211104027.json";
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    string jsonData = File.ReadAllText(filePath);
+
+                    DataMahasiswa mahasiswa = JsonConvert.DeserializeObject<DataMahasiswa>(jsonData);
+
+                    Console.WriteLine($"Nama {mahasiswa.Nama.Depan} {mahasiswa.Nama.Belakang} dengan nim {mahasiswa.NIM} dari fakultas {mahasiswa.Fakultas}");
+                }
+                catch (JsonReaderException ex)
+                {
+                    Console.WriteLine("Terjadi kesalahan saat membaca JSON: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File JSON tidak ditemukan!");
+            }
+        }
+
+        static void Main()
+        {
+            ReadJSON();
         }
     }
 }
